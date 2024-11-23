f885347d8fdee87e3fc9c6e757cdc7a2e695fad2
Ayoub Akritah <aakritah@c1r8p7.1337.ma>
2024-11-23 15:24:55 +0100

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,11 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 20:26:24 by Noctis            #+#    #+#             */
-/*   Updated: 2024/11/23 13:40:48 by aakritah         ###   ########.fr       */
+/*   Updated: 2024/11/23 15:22:28 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_get_line(char *str)
-{
-	char	*t;
-	int		i;
-	int		j;
-
-	i = 0;
-	if (!str)
-		return (NULL);
-	while (str[i] && str[i] != '\n')
-		i++;
-	t = malloc(++i + 1);
-	if (!t)
-		return (free(str), str = NULL, NULL);
-	j = 0;
-	while (j < i)
-	{
-		t[j] = str[j];
-		j++;
-	}
-	t[j] = '\0';
-	return (t);
-}
 
 char	*ft_fix_str(char *str)
 {
@@ -44,12 +20,10 @@ char	*ft_fix_str(char *str)
 
 	i = 0;
 	j = 0;
-	if (!str)
-		return (NULL);
 	while (str[i] && str[i] != '\n')
 		i++;
 	if (str[i] == '\0')
-		return (free(str),str=NULL,NULL);
+		return (free(str), str = NULL, NULL);
 	i++;
 	while (str[i + j])
 		j++;
@@ -63,31 +37,55 @@ char	*ft_fix_str(char *str)
 		j++;
 	}
 	t[j] = '\0';
-	free(str);
+	return (free(str), str = NULL, t);
+}
+
+char	*ft_get_line(char *str)
+{
+	char	*t;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	t = malloc(i + 1);
+	if (!t)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		t[j] = str[j];
+		j++;
+	}
+	t[j] = '\0';
 	return (t);
 }
 
 char	*ft_get_str(int fd, char *str)
 {
 	char	*buffer;
-	char	*t;
 	ssize_t	rs;
 
-	rs = 1;
+	if (!str)
+		str = ft_strdup("");
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
-		return (free(str), str = NULL, str);
-	while (rs != 0 && !ft_strchr(str, '\n'))
+		return (free(str), str = NULL, NULL);
+	rs = 1;
+	while (rs > 0)
 	{
 		rs = read(fd, buffer, BUFFER_SIZE);
-		if (rs == -1)
-			return (free(buffer), buffer = NULL, free(str), str = NULL, str);
-		if (rs == 0 && (!str || str[0] == '\0'))
-			return (free(buffer), free(str), NULL);
+		if (rs < 1 && (!str || !str[0]))
+			return (free(buffer), buffer = NULL, free(str), str = NULL, NULL);
 		buffer[rs] = '\0';
-		t = str;
 		str = ft_strjoin(str, buffer);
-		free(t);
+		if (!str)
+			return (NULL);
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
 	return (free(buffer), buffer = NULL, str);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 20:26:18 by Noctis            #+#    #+#             */
-/*   Updated: 2024/11/23 12:25:43 by aakritah         ###   ########.fr       */
+/*   Updated: 2024/11/23 15:22:51 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	s = ft_strlen(s1) + ft_strlen(s2);
 	t = malloc(s + 1);
 	if (!t)
-		return (NULL);
+		return (free(s1), s1 = NULL, free(s2), s2 = NULL, NULL);
 	i = 0;
 	while (s1[i] != '\0')
 	{
@@ -35,34 +35,8 @@ char	*ft_strjoin(char *s1, char *s2)
 	while (s2[j] != '\0')
 		t[i++] = s2[j++];
 	t[i] = '\0';
-	return (t);
+	return (free(s1), s1 = NULL, t);
 }
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	size_t	i;
-// 	size_t	s1;
-// 	char	*dst;
-
-// 	if (!s)
-// 		return (NULL);
-// 	i = 0;
-// 	if ((unsigned int)ft_strlen(s) <= start)
-// 		return (ft_strdup(""));
-// 	s1 = ft_strlen(s + start);
-// 	if (len < s1)
-// 		s1 = len;
-// 	dst = malloc((s1 + 1) * 1);
-// 	if (!dst)
-// 		return (NULL);
-// 	while (i < s1)
-// 	{
-// 		dst[i] = s[start + i];
-// 		i++;
-// 	}
-// 	dst[i] = '\0';
-// 	return (dst);
-// }
 
 char	*ft_strdup(const char *s1)
 {
@@ -91,8 +65,8 @@ char	*ft_strchr(const char *s, int c)
 	size_t	s1;
 	size_t	i;
 
-	if(!s)
-		return NULL;
+	if (!s)
+		return (NULL);
 	s1 = ft_strlen(s);
 	i = 0;
 	while (i < s1 + 1)
@@ -113,4 +87,3 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
-
